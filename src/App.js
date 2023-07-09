3d23749e7690acacbee12b471a10112f2aff186a
SG_Pramod <pramod.v@skysoft.net.in>
2023-07-09 19:44:48 +0530

    font issue fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { ThemeProvider} from '@mui/material/styles';
 import theme from './assets/theme/theme';
 import './App.css';
-import HomePage from './pages/homePage';
+import './font.css';
+import HomePage from './pages/home';
 function App() {
   return (
     <React.Fragment>
